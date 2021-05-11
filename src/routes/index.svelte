816ca84ffcf2dc738abd4c93e6ea8601533ccac2
Al Murray <2515630+rogueyoshi@users.noreply.github.com>
2021-05-11 19:39:07 -0400

    ES6 branch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	let mounted;
+	let aframe;
+	let ar;
+	$: ready = aframe && ar && mounted;
+
+	const aframeLoaded = () => {
+		console.log('aframe');
+		aframe = true;
+	};
+
+	const arLoaded = () => {
+		console.log('ar');
+		ar = true;
+	};
+
+	onMount(() => {
+		console.log('mounted');
+		mounted = true;
+	});
+</script>
+
+<svelte:head>
+	{#if mounted}
+		<script
+			src="https://aframe.io/releases/1.0.0/aframe.min.js"
+			on:load={aframeLoaded}></script>
+		<script
+			src="https://raw.githack.com/AR-js-org/AR.js/ES6/aframe/build/aframe-ar.js"
+			on:load={arLoaded}></script>
+	{/if}
+</svelte:head>
+
+{#if ready}
+	<a-scene
+		embedded
+		arjs="sourceWidth: window.innerWidth > window.innerHeight ? 640 : 480; sourceHeight: window.innerWidth > window.innerHeight ? 480 : 640"
+	>
+		<a-marker preset="hiro">
+			<a-box
+				position="0 0.5 0"
+				material="opacity: 0.5; side: double;color:blue;"
+			>
+				<a-torus-knot
+					radius="0.26"
+					radius-tubular="0.05"
+					animation="property: rotation; to:360 0 0; dur: 5000; easing: linear; loop: true"
+				/>
+			</a-box>
+		</a-marker>
+		<a-entity camera />
+	</a-scene>
+{/if}
+
+<style>
+	:global(html, body) {
+		height: 100%;
+		margin: 0;
+		overflow: hidden;
+	}
+
+	:global(body) {
+		position: relative;
+	}
+
+	:global(.a-canvas, #arjs-video) {
+		display: block;
+		width: 100% !important;
+		height: 100% !important;
+		margin: 0 !important;
+		object-fit: contain !important;
+	}
+</style>
---
+++ src/routes/remarkable.svelte
@@ -0,0 +1,26 @@
+<script>
+	let md;
+	let value = 'Hello, **world**!';
+
+	const initializeRemarkable = () => {
+		md = new window.remarkable.Remarkable();
+	};
+</script>
+
+<svelte:head>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/remarkable/2.0.0/remarkable.min.js"
+		on:load={initializeRemarkable}></script>
+</svelte:head>
+
+<div className="MarkdownEditor">
+	<h3>Input</h3>
+	<label htmlFor="markdown-content"> Enter some markdown </label>
+	<textarea id="markdown-content" bind:value />
+	<h3>Output</h3>
+	<div className="content">
+		{#if md}
+			{@html md.render(value)}
+		{/if}
+	</div>
+</div>
